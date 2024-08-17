4a5e0534255b2637a58e6fdff96a3bf2f1a1d9d9
santiagoe16 <santiagoespinal333@gmail.com>
2024-08-17 21:57:14 +0000

    random card when the page is refreshed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♥", "♠", "♣", "♦"];
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let suitRandom = suits[Math.floor(Math.random() * suits.length)];
+  let cardNumberRandom =
+    cardNumber[Math.floor(Math.random() * cardNumber.length)];
+
+  document.querySelector("body").innerHTML = `
+    <div class="d-flex justify-content-center mt-5">
+      <div class="bg-light card-container  text-center rounded">
+        <div class="text-start ps-3">
+          ${
+            suitRandom == "♦" || suitRandom == "♥"
+              ? `<p class= "text-danger">${suitRandom}</p>`
+              : `<p>${suitRandom}</p>`
+          }
+        </div>
+        <p>${cardNumberRandom}</p>
+        <div class="text-end pe-3">
+          ${
+            suitRandom == "♦" || suitRandom == "♥"
+              ? `<p class= "text-danger mb-1">${suitRandom}</p>`
+              : `<p>${suitRandom}</p>`
+          }
+        </div>
+      </div>
+    </div>`;
 };
